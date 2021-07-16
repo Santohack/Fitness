d57a6b7f51a09b4641f3b4517b6e046e86191539
sushir pandey <s.pandey@varshyl.com>
2021-07-16 22:15:56 +0530

    Change in file

--- Login/bmi/update_bmi.php
+++ Login/bmi/update_bmi.php
@@ -5,6 +5,6 @@ include '../conn.php';
 if(!empty($_POST)){
 	$user_id = $_SESSION['user_id'];
 	$bmi = $_POST['bmi'];
-	$eails ="Update register set bmi= $bmi, break_fast_response='', lunch_response = '', dinner_response= '' WHERE id =$user_id";
+	$eails ="Update register set bmi= '".$bmi."', break_fast_response='', lunch_response = '', dinner_response= '' WHERE id =$user_id";
 	$ql = mysqli_query($connection,$eails);
 }
